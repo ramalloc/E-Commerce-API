0217fb58a58c34636b74108fec12b4e96e76dfc6
ramalloc <roshangupta1887@gmail.com>
2023-06-03 22:45:40 +0530

    Created Get all Users in User Routes

--- Routes/Users.js
+++ Routes/Users.js
@@ -1,5 +1,5 @@
 const User = require('../Models/User');
-const { verifyToken, verifyTokenAndAutharization } = require('./verifyToken');
+const { verifyToken, verifyTokenAndAutharization, verifyTokenAndAdmin } = require('./verifyToken');
 
 const router = require('express').Router();
 
@@ -9,7 +9,6 @@ const router = require('express').Router();
 // router.put("/:id", verifyToken, (req, res) => { // we used put here because we are updating here. And We need a middleware to verify our json id (Verify Token)
 //     // So first we have to check that wheather the id is belongs to clients/admin or not.
 //     // if(req.user.id === req.params.id || req.user.isAdmin){
-
 //     // } // If we write it like this then we have to create it again and again, therefore we create this function in verifyToken.js and import it.
 // }); 
 
@@ -19,20 +18,65 @@ router.put("/:id", verifyToken, verifyTokenAndAutharization, async (req, res) =>
         req.body.password = CryptoJS.AES.encrypt(
             req.body.password,
             process.env.PASS_SEC
-            ).toString();
+        ).toString();
     }
+     
 
     // Now we have to update the requests on server
     try {
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, { // finByIdAndUpdate is a mongoDB method use to set or update
-            $set: req.body // This will take everything inside body and set it again. Bugt this will not return your updated user, To prevent this we use {new: true} 
-        },{new: true}
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id, { // finByIdAndUpdate is a mongoDB method use to set or update
+            $set: req.body // This will take everything inside body and set it again. But this will not return your updated user, To prevent this we use {new: true} 
+        }, { new: true }
         );
         // To send update user
         res.status(200).json(updatedUser);
-    } catch(err){
+    } catch (err) {
         res.status(500).json(err);
-    } 
+    }
+});
+
+
+
+// DELETE USER
+router.delete("/:id", verifyTokenAndAutharization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+// GET USER 
+// Only admin can see how many users are there.
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        // We want to send user details to the admin but not the password of the users, So to prevent this, We use pread operator with others
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others)
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+// GET ALL USERS 
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
