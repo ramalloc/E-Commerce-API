f6310b41d872a66f0b66803068804bfeb0915a2c
ramalloc <roshangupta1887@gmail.com>
2023-06-09 23:56:54 +0530

    Updated Pay and Success

---
+++ React App/pay-and-success/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Pay from './Pages/Pay';
+import Success from './Pages/Success';
+
+function App() {
+  return (
+    <Routes>
+    <Pay />
+    <Success />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ React App/pay-and-success/src/Pages/Pay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pay = () => {
+  return (
+    <button>
+      Pay Now
+    </button>
+  )
+}
+
+export default Pay
