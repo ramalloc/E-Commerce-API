31b97932a269344d9f4897eb428dd2c0edc0ad98
ramalloc <roshangupta1887@gmail.com>
2023-06-09 23:41:19 +0530

    Again Updated the stripe Route

--- Routes/Stripe.js
+++ Routes/Stripe.js
@@ -1,21 +1,26 @@
-const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const router = require('express').Router();
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
 
 router.post("/payment", (req, res) => {
-  stripe.charges.create(
-    {
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: "usd",
+    stripe.charges.create({
+        source: req.body.tokenId,
+        amount: req.body.amount,
+        currency: "inr",
     },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json(stripeErr);
-      } else {
-        res.status(200).json(stripeRes);
-      }
-    }
-  );
+        // After the above object this will return error or successful response
+        // This is a returning function
+        (stripeErr, stripeRes) => {
+            if (stripeErr) {
+                res.status(500).json(stripeErr);
+            }
+            else {
+                res.status(200).json(stripeRes);
+            }
+        }
+    );
+
 });
+
 
 module.exports = router;
